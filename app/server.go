4cd2d44af69cdd05aa83497bc713c6b355ccf2f1
Oli Knight <olidknight@gmail.com>
2024-07-14 22:10:44 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -27,13 +27,20 @@ func main() {
 		conn.Read(req)
 
 		reqStr := string(req)
+		path := strings.Split(reqStr, " ")[1]
+
 		if strings.Contains(reqStr, "GET / HTTP/1.1") {
 			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
-		} else {
+		} else if strings.Split(path, "/")[1] == "echo" {
+			msg := strings.Split(path, "/")[2]
+			returnValue := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg)
+			conn.Write([]byte(returnValue))
 
+		} else {
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		}
+		conn.Close()
 
 	}
 
