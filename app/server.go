aa74ce8923e7863cb252caa77f5a8fde90a16584
Oli Knight <olidknight@gmail.com>
2024-07-19 00:18:09 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -36,6 +36,7 @@ func handle_connection(conn net.Conn) {
 	reqStr := string(req)
 
 	split_req := strings.Split(reqStr, "\r\n")
+
 	request_line := split_req[0]
 
 	headers := make(map[string]string)
@@ -46,48 +47,73 @@ func handle_connection(conn net.Conn) {
 			header_key = header_key[:len(header_key)-1]
 
 			header_val := strings.Split(l, " ")[1]
-			fmt.Println(header_val)
 			headers[header_key] = header_val
 
 		}
 	}
 
-	path := strings.Split(request_line, " ")[1]
+	reqSplit := strings.Split(request_line, " ")
+	method := reqSplit[0]
+	fmt.Println(method)
+	path := reqSplit[1]
 	path_split := strings.Split(path, "/")
 
 	return_val := ""
 
-	switch {
-	case path == "/":
-		return_val = "HTTP/1.1 200 OK\r\n\r\n"
-		break
-	case path_split[1] == "echo":
-		echo_val := strings.Split(path, "/")[2]
-		return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo_val), echo_val)
-		break
-	case path_split[1] == "user-agent":
-		return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(headers["User-Agent"]), headers["User-Agent"])
-		break
-	case path_split[1] == "files":
+	switch method {
+	case "GET":
+		switch {
+		case path == "/":
+			return_val = "HTTP/1.1 200 OK\r\n\r\n"
+			break
+		case path_split[1] == "echo":
+			echo_val := strings.Split(path, "/")[2]
+			return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo_val), echo_val)
+			break
+		case path_split[1] == "user-agent":
+			return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(headers["User-Agent"]), headers["User-Agent"])
+			break
+		case path_split[1] == "files":
 
-		fileRootDir := os.Args[2]
+			fileRootDir := os.Args[2]
 
-		filePath := strings.Split(path, "/")[2]
-		fullPath := fmt.Sprintf("%v%s", fileRootDir, filePath)
-		fmt.Println(fullPath)
-		file, err := os.ReadFile(fullPath)
-		if err != nil {
-			fmt.Println("Error reading file")
+			filePath := strings.Split(path, "/")[2]
+			fullPath := fmt.Sprintf("%v%s", fileRootDir, filePath)
+			fmt.Println(fullPath)
+			file, err := os.ReadFile(fullPath)
+			if err != nil {
+				fmt.Println("Error reading file")
+				return_val = "HTTP/1.1 404 Not Found\r\n\r\n"
+				break
+			}
+
+			contentLength := len(file)
+			content := string(file)
+			return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)
+			break
+		default:
 			return_val = "HTTP/1.1 404 Not Found\r\n\r\n"
 			break
 		}
-
-		contentLength := len(file)
-		content := string(file)
-		return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)
 		break
-	default:
-		return_val = "HTTP/1.1 404 Not Found\r\n\r\n"
+	case "POST":
+		switch {
+		case path_split[1] == "files":
+			body := strings.Split(split_req[len(split_req)-1], "\x00")[0]
+
+			fileRootDir := os.Args[2]
+
+			filePath := strings.Split(path, "/")[2]
+			fullPath := fmt.Sprintf("%v%s", fileRootDir, filePath)
+			err := os.WriteFile(fullPath, []byte(body), 0644)
+			if err != nil {
+				fmt.Println("Error writing to file")
+				return_val = "HTTP/1.1 404 Not Found\r\n\r\n"
+				break
+			}
+			return_val = "HTTP/1.1 201 Created\r\n\r\n"
+			break
+		}
 		break
 	}
 
