ecf68b3005ff6425ab9c980f81d7bc3ac2e351f2
Oli Knight <olidknight@gmail.com>
2024-07-18 23:20:00 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -57,20 +57,40 @@ func handle_connection(conn net.Conn) {
 
 	return_val := ""
 
-	if path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-
-	} else if path_split[1] == "echo" {
+	switch {
+	case path == "/":
+		return_val = "HTTP/1.1 200 OK\r\n\r\n"
+		break
+	case path_split[1] == "echo":
 		echo_val := strings.Split(path, "/")[2]
 		return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo_val), echo_val)
-
-	} else if path_split[1] == "user-agent" {
-
+		break
+	case path_split[1] == "user-agent":
 		return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(headers["User-Agent"]), headers["User-Agent"])
+		break
+	case path_split[1] == "files":
 
-	} else {
+		fileRootDir := os.Args[2]
+
+		filePath := strings.Split(path, "/")[2]
+		fullPath := fmt.Sprintf("%v%s", fileRootDir, filePath)
+		fmt.Println(fullPath)
+		file, err := os.ReadFile(fullPath)
+		if err != nil {
+			fmt.Println("Error reading file")
+			return_val = "HTTP/1.1 404 Not Found\r\n\r\n"
+			break
+		}
+
+		contentLength := len(file)
+		content := string(file)
+		return_val = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)
+		break
+	default:
 		return_val = "HTTP/1.1 404 Not Found\r\n\r\n"
+		break
 	}
+
 	conn.Write([]byte(return_val))
 
 }
